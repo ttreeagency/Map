112fdd502d661f7d0972c1d56e2aaae5a8985e9e
Dominique Feyer <dfeyer@ttree.ch>
2015-06-08 23:19:39 +0200

    [TASK] Listen to NodeCreated JS event

--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -2,4 +2,7 @@ if (typeof document.addEventListener === 'function') {
 	document.addEventListener('Neos.PageLoaded', function (e) {
 		TtreeMap.initialize();
 	}, false);
+	document.addEventListener('Neos.NodeCreated', function (e) {
+		TtreeMap.initialize();
+	}, false);
 }
