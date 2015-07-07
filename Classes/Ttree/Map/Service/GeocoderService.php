7b776536b721d348358cea0df35b4f18fe2a3dca
Dominique Feyer <dfeyer@ttree.ch>
2015-07-07 11:56:19 +0200

    [TASK] Catch NoResult exception in GeocoderService

---
+++ Classes/Ttree/Map/Service/GeocoderService.php
@@ -0,0 +1,62 @@
+<?php
+namespace Ttree\Map\Service;
+
+use Geocoder\Exception\NoResult;
+use Geocoder\Provider\GoogleMaps;
+use Ivory\HttpAdapter\CurlHttpAdapter;
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Geocoder Service
+ *
+ * @Flow\Scope("singleton")
+ * @api
+ */
+class GeocoderService {
+
+	/**
+	 * @var GoogleMaps
+	 */
+	protected $geocoder;
+
+	/**
+	 * @var array
+	 */
+	protected $runtimeCache = [];
+
+	/**
+	 * @return void
+	 */
+	public function initializeObject() {
+		$curl = new CurlHttpAdapter();
+		$this->geocoder = new GoogleMaps($curl);
+	}
+
+	/**
+	 * @param string $address
+	 * @return array
+	 */
+	public function geocode($address) {
+		try {
+			$cacheKey = md5($address);
+			if (isset($this->runtimeCache[$cacheKey])) {
+				return $this->runtimeCache[$cacheKey];
+			}
+			$coordinates = $this->geocoder->geocode(trim($address));
+			if (!$coordinates->count()) {
+				return NULL;
+			}
+			$address = $coordinates->first();
+
+			$data = [
+				'longitude' => $address->getLongitude(),
+				'latitude' => $address->getLatitude()
+			];
+			$this->runtimeCache[$cacheKey] = $data;
+			return $data;
+		} catch (NoResult $exception) {
+			return NULL;
+		}
+	}
+
+}
