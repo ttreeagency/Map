b437a49f473877e68f07f44e21c19372e45ab99c
Pfaffenrodt <dimitri@pfaffenrodt.de>
2017-11-10 01:37:41 +0100

    [BUGFIX] support setting of apiKey and remove jQuery as dependency

--- Resources/Public/JavaScript/Map.js
+++ Resources/Public/JavaScript/Map.js
@@ -1,39 +1,31 @@
 TtreeMap = {
-	initializeSingleMap: function(map, configuration) {
-		var mapConfiguration = configuration.map,
-			mapOptions = mapConfiguration.options,
-			myLatlng = new google.maps.LatLng(mapConfiguration.latitude, mapConfiguration.longitude);
+    initializeSingleMap: function(map, configuration) {
+        var mapConfiguration = configuration.map,
+            mapOptions = mapConfiguration.options,
+            myLatlng = new google.maps.LatLng(mapConfiguration.latitude, mapConfiguration.longitude);
 
-		mapOptions.center = myLatlng;
-		mapOptions.mapTypeControlOptions = {
-			mapTypeIds: [google.maps.MapTypeId.ROADMAP, "styles"]
-		};
+        mapOptions.center = myLatlng;
+        mapOptions.mapTypeControlOptions = {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, "styles"]
+        };
 
-		var map = new google.maps.Map(map, mapOptions),
-			mapType = new google.maps.StyledMapType(configuration.styles.default, {});
+        var map = new google.maps.Map(map, mapOptions),
+            mapType = new google.maps.StyledMapType(configuration.styles.default, {});
 
-		map.mapTypes.set("styles", mapType);
-		map.setMapTypeId("styles");
+        map.mapTypes.set("styles", mapType);
+        map.setMapTypeId("styles");
 
-		new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	},
-	initialize: function() {
-		if (typeof $ !== 'undefined') {
-			$(".ttree-map-configuration-js").each(function () {
-				var el = $(this),
-					configuration = JSON.parse(decodeURIComponent(el.data('ttreeMapConfiguration')));
-
-				TtreeMap.initializeSingleMap(el[0], configuration);
-			});
-		}
-	}
+        new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    },
+    initialize: function() {
+        var entries = document.getElementsByClassName("ttree-map-configuration-js");
+        for(var index = 0; index < entries.length; index++) {
+            var entry = entries[index];
+            var configuration = JSON.parse(decodeURIComponent(entry.getAttribute('data-ttree-map-configuration')));
+            TtreeMap.initializeSingleMap(entry, configuration);
+        }
+    }
 };
-
-if (typeof $ !== 'undefined') {
-	$(document).ready(function () {
-		google.maps.event.addDomListener(window, 'load', TtreeMap.initialize);
-	});
-}
